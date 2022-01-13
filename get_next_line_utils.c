a2359631bb5e2f6a76e0bf563b318635048a6aeb
yeseul <letmeshare46@gmail.com>
2022-01-13 14:24:48 +0900

    add_buf에서 save가 널이면 save가 아닌 buf를 복사했어야지

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,5 +1,27 @@
 #include "get_next_line.h"
 
+char	*add_buf(char const *save, char const *buf)
+{
+	char	*new;
+
+	if (buf == NULL)
+		return (NULL);
+	else if (save == NULL)
+	{
+		new = malloc(ft_strlen(buf) + 1);
+		if (new == NULL)
+			return (NULL);
+		ft_strlcpy(new, buf, ft_strlen(buf) + 1);
+		return (new);
+	}
+	new = malloc(ft_strlen(save) + ft_strlen(buf) + 1);
+	if (new == NULL)
+		return (NULL);
+	ft_strlcpy(new, save, ft_strlen(save) + 1);
+	ft_strlcpy(new + ft_strlen(save), buf, ft_strlen(buf) + 1);
+	return (new);
+}
+
 size_t	ft_strlen(const char *s)
 {
 	size_t	len;
@@ -43,26 +65,4 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 		dst[i] = '\0';
 	}
 	return (ft_strlen(src));
-}
-
-char	*add_buf(char const *save, char const *buf)
-{
-	char	*new;
-
-	if (buf == NULL)
-		return (NULL);
-	else if (save == NULL)
-	{
-		new = malloc(ft_strlen(save) + 1);
-		if (new == NULL)
-			return (NULL);
-		strlcpy(new, save, ft_strlen(save) + 1);
-		return (new);
-	}
-	new = malloc(ft_strlen(save) + ft_strlen(buf) + 1);
-	if (new == NULL)
-		return (NULL);
-	ft_strlcpy(new, save, ft_strlen(save) + 1);
-	ft_strlcpy(new + ft_strlen(save), buf, ft_strlen(buf) + 1);
-	return (new);
 }
