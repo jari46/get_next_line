9aada2fef2906ce381f1035ffaa23e003ee0ba73
yeseul <letmeshare46@gmail.com>
2022-01-13 14:26:23 +0900

    변경된 함수 추가

--- get_next_line.h
+++ get_next_line.h
@@ -8,8 +8,8 @@
 char    *get_next_line(int fd);
 char    *read_file(char *save, int fd);
 char    *get_line(char *save);
+char	*reset_save(char **save, int offset);
+char	*add_buf(char const *save, char const *buf);
 size_t	ft_strlen(const char *s);
 char    *ft_strchr(const char *s, int c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, char const *s2);
